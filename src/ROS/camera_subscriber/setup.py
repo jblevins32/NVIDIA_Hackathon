a999ef284b70817da69b346da9814d0659251dab
Adam <adam.mazlout@gatech.edu>
2025-01-13 16:48:06 -0500

    New camera_subscriber folder

--- src/ROS/camera_subscriber/setup.py
+++ src/ROS/camera_subscriber/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'camera_subpub'
+package_name = 'py_pubsub'
 
 setup(
     name=package_name,
@@ -11,17 +11,18 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    # install_requires=['setuptools','rclpy','cv_bridge','python3-opencv'],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hkwon82',
-    maintainer_email='hkwon82@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='jblevins32',
+    maintainer_email='jacob.blevins@gatech.edu',
+    description='Examples of minimal publisher/subscriber using rclpy',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'publisher = camera_subpub.camera_publisher:main',
-            'subscriber = camera_subpub.camera_subscriber:main',
+                'talker = py_pubsub.publisher_member_function:main',
+                'listener = py_pubsub.subscriber_member_function:main',
         ],
     },
 )
+
